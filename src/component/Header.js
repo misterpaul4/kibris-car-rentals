18cee9677b369a19ceced95103f27efd2379ec68
Paul Ajuizeogu <ajuzepaul@gmail.com>
2021-12-17 05:47:07 +0200

    Add header icons (menu and search)

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,22 +1,20 @@
 import React from 'react';
+import menuIcon from '../img/menu.svg';
+import searchIcon from '../img/search.svg';
 import '../css/Header.css';
 
 const Header = () => {
 
   return (
   <header className='p-2 border bg-light'>
-    <div className='d-flex justify-content-between align-items-center'>
-      <div>
-        menu
-      </div>
+    <div className='d-flex justify-content-between align-items-center px-3'>
+      <img src={menuIcon} alt='menu icon'></img>
 
       <h4 className='align-self-end'>
-        Tit
+        Cars
       </h4>
 
-      <div className='rounded border py-2 px-2'>
-        AV
-      </div>
+      <img src={searchIcon} alt='search icon'></img>
     </div>
   </header>
   )
